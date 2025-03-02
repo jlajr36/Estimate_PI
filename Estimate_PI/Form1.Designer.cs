d78726a5f1266992b8c38383819258a4d507344d
horiz <horiz@Horizon.com>
2025-03-01 22:21:12 -0500

    Simaple working drawing.

--- Estimate_PI/Form1.Designer.cs
+++ Estimate_PI/Form1.Designer.cs
@@ -28,13 +28,47 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.skglCanvas = new SkiaSharp.Views.Desktop.SKGLControl();
+            this.btnEst = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // skglCanvas
+            // 
+            this.skglCanvas.BackColor = System.Drawing.Color.Black;
+            this.skglCanvas.Location = new System.Drawing.Point(12, 12);
+            this.skglCanvas.Name = "skglCanvas";
+            this.skglCanvas.Size = new System.Drawing.Size(677, 426);
+            this.skglCanvas.TabIndex = 0;
+            this.skglCanvas.VSync = false;
+            this.skglCanvas.PaintSurface += new System.EventHandler<SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs>(this.skglCanvas_PaintSurface);
+            // 
+            // btnEst
+            // 
+            this.btnEst.Location = new System.Drawing.Point(698, 12);
+            this.btnEst.Name = "btnEst";
+            this.btnEst.Size = new System.Drawing.Size(93, 23);
+            this.btnEst.TabIndex = 1;
+            this.btnEst.Text = "Estimate PI";
+            this.btnEst.UseVisualStyleBackColor = true;
+            this.btnEst.Click += new System.EventHandler(this.btnEst_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.btnEst);
+            this.Controls.Add(this.skglCanvas);
+            this.Name = "Form1";
+            this.Text = "Estimate PI";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private SkiaSharp.Views.Desktop.SKGLControl skglCanvas;
+        private System.Windows.Forms.Button btnEst;
     }
 }
 
--- Estimate_PI/Form1.cs
+++ Estimate_PI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SkiaSharp;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,47 @@ namespace Estimate_PI
 {
     public partial class Form1 : Form
     {
+        bool paint = false;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void skglCanvas_PaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintGLSurfaceEventArgs e)
+        {
+            if (paint)
+            {
+                SKSurface surface = e.Surface;
+                SKCanvas canvas = surface.Canvas;
+
+                // Clear the canvas to a background color
+                canvas.Clear(SKColor.Parse("#003366"));
+
+                // Create a paint object
+                using (SKPaint paint = new SKPaint())
+                {
+                    paint.Color = SKColors.White;
+                    paint.StrokeWidth = 5;
+
+                    // Draw a circle
+                    canvas.DrawCircle(200, 200, 50, paint);
+                }
+
+                paint = false;
+            }
+        }
+
+        private void doDraw()
+        {
+            skglCanvas.Invalidate();
+            Application.DoEvents();
+        }
+
+        private void btnEst_Click(object sender, EventArgs e)
+        {
+            paint = true;
+            doDraw();
         }
     }
 }
